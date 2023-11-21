f1750607ce68b7cfa55277f7ed730d8e0b18383d
Ayesha-chaudhry <ashijutt069@gmail.com>
2023-11-22 01:51:01 +0500

    api update

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,21 +1,117 @@
 
 "use client"
-import { useState } from "react";
-import {
-  Container,
-  Heading,
-  Button,
-  FormControl,
-  FormLabel,
-  Input,
-  VStack,
-} from "@chakra-ui/react";
-import axios from "axios";
+// import { useState } from "react";
+// import {
+//   Container,
+//   Heading,
+//   Button,
+//   FormControl,
+//   FormLabel,
+//   Input,
+//   VStack,
+// } from "@chakra-ui/react";
+// import axios from "axios";
+
+// const Home: React.FC = () => {
+//   const [file, setFile] = useState<File | null>(null);
+//   const [fileName, setFileName] = useState<string>("");
+//   const [fileType, setFileType] = useState<string>("text/plain");
+
+//   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     const selectedFile = event.target.files?.[0];
+//     if (selectedFile) {
+//       setFile(selectedFile);
+//       setFileName(selectedFile.name);
+//     }
+//   };
+
+//   const handleUpload = async () => {
+//     try {
+//       const apiUrl ='https://zp2dhmgwaa.execute-api.us-east-1.amazonaws.com/generatepresignedurl';
+
+//       const response = await axios.get(apiUrl, {
+//         params: {
+//           fileName: fileName,
+//           contentType: fileType,
+//         },
+//       });
+
+//       if (response.status === 200) {
+//         const { signedUrl } = response.data.uploadUrl;
+//         console.log("pre signed url", response.data.uploadUrl);
+
+//         const fileContent = await file?.arrayBuffer();
+//         console.log("fileContent:", fileContent)
+//         if (fileContent) {
+//           const uploadResponse = await axios.put(signedUrl, fileContent, {
+//             headers: {
+//               'Content-Type': fileType,
+//             },
+//           });
+
+//           if (uploadResponse.status === 200) {
+//             console.log('File uploaded successfully');
+//             // Add any logic to handle success, e.g., showing a success message
+//           } else {
+//             console.error('File upload failed:', uploadResponse.statusText);
+//           }
+//         }
+//       } else {
+//         console.error("Failed to get pre-signed URL:", response.statusText);
+//       }
+//     } catch (error) {
+//       console.error("Error uploading file:", error);
+//     }
+    
+//   };
+
+//   return (
+//     <Container maxW="container.sm" centerContent>
+//       <Heading as="h1" mb={6}>
+//         S3 File Upload
+//       </Heading>
+
+//       <VStack spacing={4} align="stretch">
+//         <FormControl>
+//           <FormLabel>Choose File</FormLabel>
+//           <Input type="file" onChange={handleFileChange} />
+//         </FormControl>
+
+//         {file && (
+//           <>
+//             <FormControl>
+//               <FormLabel>File Name</FormLabel>
+//               <Input type="text" value={fileName} isReadOnly />
+//             </FormControl>
+
+//             <FormControl>
+//               <FormLabel>File Type</FormLabel>
+//               <Input type="text" value={fileType} isReadOnly />
+//             </FormControl>
+
+//             <Button colorScheme="blue" variant={"ghost"} onClick={handleUpload}>
+//               Upload File
+//             </Button>
+//           </>
+//         )}
+//       </VStack>
+//     </Container>
+//   );
+// };
+
+// export default Home;
+
+
+
+
+import { useState } from 'react';
+import { Container, Heading, Button, FormControl, FormLabel, Input, VStack } from '@chakra-ui/react';
+import axios from 'axios';
 
 const Home: React.FC = () => {
   const [file, setFile] = useState<File | null>(null);
-  const [fileName, setFileName] = useState<string>("");
-  const [fileType, setFileType] = useState<string>("text/plain");
+  const [fileName, setFileName] = useState<string>('');
+  const [fileType, setFileType] = useState<string>('text/plain');
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = event.target.files?.[0];
@@ -27,7 +123,7 @@ const Home: React.FC = () => {
 
   const handleUpload = async () => {
     try {
-      const apiUrl ='https://zp2dhmgwaa.execute-api.us-east-1.amazonaws.com/generatepresignedurl';
+      const apiUrl = 'https://zp2dhmgwaa.execute-api.us-east-1.amazonaws.com/generatepresignedurl';
 
       const response = await axios.get(apiUrl, {
         params: {
@@ -37,29 +133,34 @@ const Home: React.FC = () => {
       });
 
       if (response.status === 200) {
-        const { signedUrl } = response.data.uploadUrl;
-        console.log("pre signed url", response.data.uploadUrl);
+        const uploadUrl = response.data?.uploadUrl;
+        console.log("Response data:", uploadUrl)
 
-        const fileContent = await file?.arrayBuffer();
-        if (fileContent) {
-          const uploadResponse = await axios.put(signedUrl, fileContent, {
-            headers: {
-              'Content-Type': fileType,
-            },
-          });
+        if (uploadUrl) {
+          const fileContent = await file?.arrayBuffer();
 
-          if (uploadResponse.status === 200) {
-            console.log('File uploaded successfully');
-            // Add any logic to handle success, e.g., showing a success message
-          } else {
-            console.error('File upload failed:', uploadResponse.statusText);
+          if (fileContent) {
+            const uploadResponse = await axios.put(uploadUrl, fileContent, {
+              headers: {
+                'Content-Type': fileType,
+              },
+            });
+
+            if (uploadResponse.status === 200) {
+              console.log('File uploaded successfully');
+              // Add any logic to handle success, e.g., showing a success message
+            } else {
+              console.error('File upload failed:', uploadResponse.statusText);
+            }
           }
+        } else {
+          console.error('Upload URL is undefined in the response:', response.data);
         }
       } else {
-        console.error("Failed to get pre-signed URL:", response.statusText);
+        console.error('Failed to get pre-signed URL:', response.statusText);
       }
     } catch (error) {
-      console.error("Error uploading file:", error);
+      console.error('Error uploading file:', error);
     }
   };
 
@@ -87,7 +188,7 @@ const Home: React.FC = () => {
               <Input type="text" value={fileType} isReadOnly />
             </FormControl>
 
-            <Button colorScheme="blue" variant={"ghost"} onClick={handleUpload}>
+            <Button colorScheme="blue" onClick={handleUpload}>
               Upload File
             </Button>
           </>
